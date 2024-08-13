9136df8079bcb97db0ee75ca88f0bee0aef4a45d
Papperla <krivman@Savar.local>
2024-08-13 10:36:46 +0200

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
+
+const nextConfig1 = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en", "ru"],
+    defaultLocale: "en",
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'staging-it-incubator.s3.eu-central-1.amazonaws.com',
+        pathname: '/trainee-instagram-api/Image/**'
+      }
+    ]
+  },
 };
 
-export default nextConfig;
+
+const nextConfig = {
+  ...nextConfig1,
+  webpack(config, options) {
+    const { webpack } = options;
+    if (!options.isServer) {
+      config.plugins.push(
+          new NextFederationPlugin({
+            name: 'messenger',
+            filename: 'static/chunks/remoteEntry.js',
+            remotes: {},
+            exposes: {
+              './messenger': './src/widgets/messenger/messenger.tsx',
+            },
+            shared: {},
+          }),
+      );
+    }
+
+    return config;
+  },
+};
+
+export default nextConfig
+
---
+++ src/widgets/messenger/messenger.tsx
@@ -0,0 +1,9 @@
+const Messenger = () => {
+    return (
+        <div>
+            <h1>WORK</h1>
+        </div>
+    );
+};
+
+export default  Messenger
